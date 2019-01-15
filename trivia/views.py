31e5ce832a3e0dfe58adfaf1fcd6515573c081af
Naflin <nathanblue00@gmail.com>
2019-01-14 21:06:23 -0500

    Trivia form and massive front page style overhaul

--- trivia/views.py
+++ trivia/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 import requests
+from .forms import TriviaAnswerForm
 
 def home(request):
-    response = requests.get("http://jservice.io/api/random")
-    tridata = response.json()
-    print(tridata)
+
+    if 'trivia-obj' in request.session:
+        tridata = request.session['trivia-obj']
+    else:
+        response = requests.get("http://jservice.io/api/random")
+        tridata = response.json()
+        request.session['trivia-obj'] = tridata
+
+    form = TriviaAnswerForm()
+    query = None
+    if 'query' in request.GET:
+        form = TriviaAnswerForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            if query == tridata[0]['answer']:
+                response = requests.get("http://jservice.io/api/random")
+                tridata = response.json()
+                request.session['trivia-obj'] = tridata
+            
+
+
     return render(request, 'trivia/home.html', {
+        'form': form,
         'id': tridata[0]['id'],
         'question': tridata[0]['question'],
         'answer': tridata[0]['answer'],
